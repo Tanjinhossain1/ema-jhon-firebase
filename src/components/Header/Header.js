68ca9bb519fdf891b535fbc04a15b32a4c89db14
tanjinhossain1 <tanjinhossain2003@gmail.com>
2022-04-09 23:28:39 +0600

    login input group and button or google button impliment done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,6 +11,7 @@ const Header = () => {
                 <Link to="/shop">Shop</Link>
                 <Link to="/orders">Orders</Link>
                 <Link to="/inventory">Inventory</Link>
+                <Link to='/login'>Login</Link>
                 <Link to="/about">About</Link>
             </div>
         </nav>
