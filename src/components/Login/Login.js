b6c4378c289cd6763f077950a6731f02a8a55d6c
tanjinhossain1 <tanjinhossain2003@gmail.com>
2022-04-11 19:47:23 +0600

    shin in and google sing in done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,38 +1,63 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 import './Login.css';
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+    const [signInWithGoogle] = useSignInWithGoogle(auth);
+
+    const [ signInWithEmailAndPassword,user ] = useSignInWithEmailAndPassword(auth);
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+    }
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+    }
+    const location = useLocation();
+    let from = location.state?.from?.pathname || "/";
+
+    if(user){
+        navigate(from)
+    }
+    const handleToSubmit = (e) => {
+        e.preventDefault();
+        signInWithEmailAndPassword(email, password)
+    }
+
     return (
-      <div className='login-container'>
+        <div className='login-container'>
             <div className='form-container'>
-            <div>
-                <h2 className='form-title'>Login</h2>
-                <form>
-                    <div className="form-group">
-                        <label htmlFor="Email">Email</label>
-                        <input type="email" name="email" id="" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password">Password</label>
-                        <input type="password" name="password" id="" />
-                    </div>
-                    <input className='submit-input' type="submit" value="Submit" />
-                </form>
-                <p>
-                    new to ema-john?<Link to='/signup'>Create New Account</Link>
-                </p>
-                <div className='or-container'>
-                    <p className='or-left'></p>
-                    <p>or</p>
-                    <p className='or-right'></p>
-                </div>
                 <div>
-                    <button className='google-button'> <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2R_7bMO9d6c7f-DPHuqUinDwpqNF00jKnrQ&usqp=CAU" alt="" /> Continue with Google</button>
+                    <h2 className='form-title'>Login</h2>
+                    <form onSubmit={handleToSubmit}>
+                        <div className="form-group">
+                            <label htmlFor="Email">Email</label>
+                            <input onBlur={handleEmail} type="email" name="email" id="" required/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input onBlur={handlePassword} type="password" name="password" id="" required/>
+                        </div>
+                        <input className='submit-input' type="submit" value="Submit" />
+                    </form>
+                    <p>
+                        new to ema-john?<Link to='/signup'>Create New Account</Link>
+                    </p>
+                    <div className='or-container'>
+                        <p className='or-left'></p>
+                        <p>or</p>
+                        <p className='or-right'></p>
+                    </div>
+                    <div>
+                        <button onClick={()=>signInWithGoogle()} className='google-button'> <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2R_7bMO9d6c7f-DPHuqUinDwpqNF00jKnrQ&usqp=CAU" alt="" /> Continue with Google</button>
+                    </div>
                 </div>
             </div>
         </div>
-      </div>
     );
 };
 
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,46 +1,63 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
+import { useCreateUserWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+
 
 const SignUp = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
     const [error, setError] = useState('');
+    const navigate = useNavigate();
+    const [signInWithGoogle] = useSignInWithGoogle(auth);
 
-    
+    const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth);
 
-    const handleEmail = (e) =>{
+    const handleEmail = (e) => {
         setEmail(e.target.value);
     }
-    const handlePassword = (e) =>{
+    const handlePassword = (e) => {
         setPassword(e.target.value);
     }
-    const handleConfirmPassword = (e) =>{
+    const handleConfirmPassword = (e) => {
         setConfirmPassword(e.target.value);
     }
+    if (user) {
+        navigate('/orders')
+    }
+    const handleToSubmit = (e) => {
+        e.preventDefault();
+        if (password !== confirmPassword) {
+            setError("password don't matches");
+            return;
+        }
+        createUserWithEmailAndPassword(email, password)
+    }
+
     return (
         <div>
             <div className='login-container'>
                 <div className='form-container'>
                     <div>
                         <h2 className='form-title'>Login</h2>
-                        <form>
+                        <form onSubmit={handleToSubmit}>
                             <div className="form-group">
                                 <label htmlFor="Email">Email</label>
-                                <input onBlur={handleEmail} type="email" name="email" id="" />
+                                <input onBlur={handleEmail} type="email" name="email" id="" required/>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="password">Password</label>
-                                <input onBlur={handlePassword} type="password" name="password" id="" />
+                                <input onBlur={handlePassword} type="password" name="password" id="" required/>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="confirm-password">Confirm-Password</label>
-                                <input onBlur={handleConfirmPassword} type="password" name="password" id="" />
+                                <input onBlur={handleConfirmPassword} type="password" name="password" id=""required />
                             </div>
                             <input className='submit-input' type="submit" value="Submit" />
                         </form>
                         <p>
-                            new to ema-john?<Link to='/signup'>Create New Account</Link>
+                            Already have an account?<Link to='/login'>Login</Link>
                         </p>
                         <div className='or-container'>
                             <p className='or-left'></p>
@@ -48,7 +65,7 @@ const SignUp = () => {
                             <p className='or-right'></p>
                         </div>
                         <div>
-                            <button className='google-button'> <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2R_7bMO9d6c7f-DPHuqUinDwpqNF00jKnrQ&usqp=CAU" alt="" /> Continue with Google</button>
+                            <button onClick={()=>signInWithGoogle()} className='google-button'> <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2R_7bMO9d6c7f-DPHuqUinDwpqNF00jKnrQ&usqp=CAU" alt="" /> Continue with Google</button>
                         </div>
                     </div>
                 </div>
