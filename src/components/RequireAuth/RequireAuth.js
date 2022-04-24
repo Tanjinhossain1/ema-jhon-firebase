83aa2963eedd4e98d48e6004fe9e19177e780572
tanjinhossain1 <tanjinhossain2003@gmail.com>
2022-04-25 00:04:35 +0600

    submit

--- src/components/RequireAuth/RequireAuth.js
+++ src/components/RequireAuth/RequireAuth.js
@@ -4,8 +4,16 @@ import { Navigate, useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 
 const RequireAuth = ({children}) => {
-    const [user] = useAuthState(auth)
+    const [user,loading] = useAuthState(auth);
     let location = useLocation();
+    if(loading){
+        return(
+            <div>
+                <h1>Loading...</h1>
+            </div>
+        )
+    }
+
     if(!user){
         return <Navigate to="/login" state={{ from: location }} replace />;
     }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+
+// const container = document.getElementById('root');
+// const root = createRoot(container);
+// root.render(<BrowserRouter>
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// </BrowserRouter>);
 
 ReactDOM.render(
   <BrowserRouter>
